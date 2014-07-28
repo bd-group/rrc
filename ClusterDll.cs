d5e58da6183fb172f9e45337c1bf70a8c67dffb6
fudaniel <fupeng.iie@gmail.com>
2014-07-28 17:09:47 +0800

    Cluster improve

--- ClusterDll.cs
+++ ClusterDll.cs
@@ -49,11 +49,19 @@ namespace ClusterLibrary
     {
         public int id;
         public HashSet<string> words;
-        public System.Int32 hashCode;
-        //public List<int> sameId;
+        public System.Int32 hashCode;       
     }
 
-    public class ClusterDll : ErrorCode
+    public class Center
+    {
+        public string clusterName;
+        public HashSet<string> words;
+        public System.Int32 hashcode;
+        public string idSet;
+        public int Count;
+    }
+
+    public class ClusterDll : ClusterInterface
     {
         //
         const string path = @"ICTCLAS50.dll";
@@ -85,9 +93,24 @@ namespace ClusterLibrary
         //
         private DataTable resultDataTable = new DataTable("ClusterResult");
         //
-       
+        private BlockingCollection<DataTable> dataTableCollection = new BlockingCollection<DataTable>();
+        //
+        private List<Center> centerList = new List<Center>();
+        //
+        private Thread splitThread;
+        //
+        private Thread clusterThread;
+        //
+        //private bool isAllFinished = false;
+        //
+        //private bool isDeliveryComplete = false;
+        //
+        //private bool isSplitComplete = false;
+        private AutoResetEvent autoEvent = new AutoResetEvent(false);
+        //private ManualResetEvent manualEvent;
+        //private BlockingCollection<List<Center>> result = new BlockingCollection<List<Center>>();
 
-
+        
         public int Init(string configFilePath)
         {
             this.filePath = configFilePath;
@@ -100,7 +123,13 @@ namespace ClusterLibrary
             if (ICTCLAS_Init(filePath))
             {
                 this.InitStopWordTable();
-
+                                
+                this.splitThread = new Thread(SpliteThread);
+                this.splitThread.Start();
+                                
+                this.clusterThread = new Thread(ClusterThread);
+                this.clusterThread.Start();
+                
                 return ErrorCode.INIT_SUCCESS;
             }
             else
@@ -126,114 +155,76 @@ namespace ClusterLibrary
 
         public int DeliveryData(DataTable dataTable)
         {
-            Stopwatch sw1 = new Stopwatch();
-            sw1.Start();
-
-            List<Sentence> mesgList = null;
-            mesgList = dataTable.AsEnumerable().AsParallel().Select(row => DataTable2Sentence(row)).ToList();
-
-            sw1.Stop();
-            Console.WriteLine("delivery data took {0}", sw1.Elapsed);
-
-            if (mesgList == null)
+            try
             {
+                this.dataTableCollection.TryAdd(dataTable);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
                 return ErrorCode.DELIVERY_DATA_FAILED;
             }
-            else
-            {
-                this.mesgBlockCollection.Add(mesgList);
-
-                
-                return ErrorCode.DELIVERY_DATA_SUCCESS;
-            }
+            return ErrorCode.DELIVERY_DATA_SUCCESS;
         }
 
-        
+            
 
         public int GetClustered(out DataTable dataTableResult)
         {
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            this.mesgBlockCollection.CompleteAdding();
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            this.dataTableCollection.CompleteAdding();
+            //this.isDeliveryComplete = true;
 
-            List<List<Sentence>> dataResult = null;
-
-            try
+            autoEvent.WaitOne();
+            /*while (!this.isAllFinished)
             {
-                Stopwatch sw2 = new Stopwatch();
-                sw2.Start();
-                List<List<Sentence>> classifiedSentenceCollection = ClassifySentence(this.mesgBlockCollection);
-                sw2.Stop();
-                Console.WriteLine("classify collection took {0}", sw2.Elapsed);
-
-                Stopwatch sw3 = new Stopwatch();
-                sw3.Start();
-                dataResult = classifiedSentenceCollection.AsParallel().Select(sList => ComputeCluster(sList)).
-                    Aggregate((list1, list2) =>
-                    {
-                        list1.AddRange(list2);
-                        return list1;
-                    });
-               
-                sw3.Stop();
-                Console.WriteLine("cluster took {0}", sw3.Elapsed);
-            }
-            catch (AggregateException ae)
-            {
-                dataTableResult = null;
-                foreach (var e in ae.InnerExceptions)
-                {
-                    Console.WriteLine("Exceptions: {0}", e.ToString());
-                }
-            }
-            sw.Stop();
-            Console.WriteLine("Time took in cluster {0}", sw.Elapsed);
-            if (this.resultDataTable == null)
+                Thread.Sleep(1000);
+            }*/
+            //Console.WriteLine("this center list count is {0}", this.centerList.Count);
+            if (this.centerList.Count == 0)
             {
                 dataTableResult = null;
                 return ErrorCode.GET_RESULT_FAILED;
             }
             else
             {
-                Stopwatch sw4 = new Stopwatch();
-                sw4.Start();
+                //Stopwatch sw4 = new Stopwatch();
+                //sw4.Start();
                 this.resultDataTable.Columns.Add(new DataColumn("ClusterName", typeof(string)));
                 this.resultDataTable.Columns.Add(new DataColumn("idSet", typeof(string)));
                 this.resultDataTable.Columns.Add(new DataColumn("count", typeof(int)));
-                foreach (List<Sentence> cluster in dataResult)
-                {
-                    string idSet = "";
-                    string clusterName = "";
-                    bool flag = true;
-                    foreach (Sentence s in cluster)
-                    {
-                        idSet += s.id.ToString() + ",";
 
-                        if (flag == true)
-                        {
-                            foreach (string st in s.words)
-                            {
-                                clusterName += st;
-                            }
-                            flag = false;
-                        }
-                    }
+                /*Parallel.ForEach(this.centerList, center =>
+                {
                     DataRow row = resultDataTable.NewRow();
-                    
-                    
-                    row["ClusterName"] = clusterName;
-                    row["idSet"] = idSet;
-                    row["count"] = cluster.Count;
+                    row["ClusterName"] = center.clusterName;
+                    row["idSet"] = center.idSet;
+                    row["count"] = center.Count;
+                    resultDataTable.Rows.Add(row);
+                });*/
+                Stopwatch s1 = new Stopwatch();
+                s1.Start();
+                foreach (Center center in this.centerList)
+                {
+                    DataRow row = resultDataTable.NewRow();
+                    row["ClusterName"] = center.clusterName;
+                    row["idSet"] = center.idSet;
+                    row["count"] = center.Count;
                     resultDataTable.Rows.Add(row);
                 }
+                s1.Stop();
+                Console.WriteLine("make datatable result take {0}", s1.Elapsed);
+
 
                 dataTableResult = this.resultDataTable;
-                sw4.Stop();
-                Console.WriteLine("make result datatable took {0}", sw4.Elapsed);
+                //sw4.Stop();
+                //Console.WriteLine("make result datatable took {0}", sw4.Elapsed);
 
                 return ErrorCode.GET_RESULT_SUCCESS;
             }
-            
+                    
+                       
         }
 
         public int AbortAll()
@@ -241,6 +232,78 @@ namespace ClusterLibrary
             return ErrorCode.ABORTALL_SUCCESS;
         }
 
+        private void SpliteThread()
+        {
+            Stopwatch s2 = new Stopwatch();
+            s2.Start();
+            while (!this.dataTableCollection.IsCompleted)
+            {
+                try
+                {
+                    DataTable data = null;
+                    this.dataTableCollection.TryTake(out data);
+                    if (data != null)
+                    {
+                        List<Sentence> mesgList = null;
+                        mesgList = data.AsEnumerable().AsParallel().Select(row => DataTable2Sentence(row)).ToList();
+                        if (mesgList != null)
+                        {
+                            this.mesgBlockCollection.Add(mesgList);
+                            /*if (this.isDeliveryComplete && this.dataTableCollection.IsCompleted)
+                            {
+                                this.isSplitComplete = true;
+                                this.mesgBlockCollection.CompleteAdding();
+                            }*/
+                        }
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+
+            }
+            this.mesgBlockCollection.CompleteAdding();
+            //this.isSplitComplete = true;
+            //Console.WriteLine("Split words complete!");
+            s2.Stop();
+            Console.WriteLine("split thread take {0}", s2.Elapsed);
+        }
+
+        private void ClusterThread()
+        {
+            Stopwatch s3 = new Stopwatch();
+            s3.Start();
+            while (!this.mesgBlockCollection.IsCompleted)
+            {                
+                try
+                {
+                    List<Sentence> sentenceList = null;
+                    this.mesgBlockCollection.TryTake(out sentenceList);
+                    
+                    if (sentenceList != null)
+                    {
+                        //Console.WriteLine("message collection take {0} and will enter ClusterProcess", i);
+                        //i++;
+                        ClusterProcess(sentenceList);
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+
+            }
+            //this.result.Add(this.centerList);
+            autoEvent.Set();
+            //if (this.isSplitComplete && this.mesgBlockCollection.IsCompleted)
+            //{
+                //this.isAllFinished = true;
+                //Console.WriteLine("is all finished");
+            //}
+            //Console.WriteLine("cluster thread end!");
+            s3.Stop();
+            Console.WriteLine("cluster thread take {0}", s3.Elapsed);
+            
+        }
        
         private HashSet<string> SplitWord(string input, out System.Int32 hashcode)
         {
@@ -274,40 +337,119 @@ namespace ClusterLibrary
             return wordList;
         }
 
-        /// <summary>
-        /// do classify
-        /// shuffle
-        /// </summary>
-        /// <param name="sentenceList">input sentences</param>
-        /// <returns>classified sentences</returns>
-        protected virtual List<List<Sentence>> ClassifySentence(BlockingCollection<List<Sentence>> bc)
+        private void ClusterProcess(List<Sentence> sentenceList)
         {
-            Stopwatch sw5 = new Stopwatch();
-            sw5.Start();
-            List<Sentence> unqueList;
-            unqueList = bc.AsParallel().Select(sList => GetUnque(sList)).
+            /*this.centerList = sentenceList.AsParallel().Select(sent => ComputeCluster(sent, this.centerList)).
                 Aggregate((list1, list2) =>
                 {
                     list1.AddRange(list2);
                     return list1;
-                });
-            sw5.Stop();
-            Console.WriteLine("unque took {0}", sw5.Elapsed);
-           
-            var ret = new List<List<Sentence>>();
-            
-            List<Sentence> sentencesLess4 = new List<Sentence>();
-            List<Sentence> sentencesOver4 = new List<Sentence>();
-
-            sentencesLess4.AddRange(unqueList.Where(s => s.words.Count <= 4).ToList());
-            sentencesOver4.AddRange(unqueList.Where(s => s.words.Count > 4).ToList());
-
-            ret.Add(sentencesOver4);
-            ret.Add(sentencesLess4);
-            Console.WriteLine("---{0}, {1}", sentencesLess4.Count, sentencesOver4.Count);
-
-            return ret;
+                });*/
+            //Console.WriteLine("sentence list length is {0}", sentenceList.Count);
+            Stopwatch s4 = new Stopwatch();
+            s4.Start();
+            foreach (Sentence s in sentenceList)
+            {
+                this.centerList = ComputeCluster(s, this.centerList);
+            }
+            s4.Stop();
+            Console.WriteLine("the core process take {0}", s4.Elapsed);
         }
+
+        private List<Center> ComputeCluster(Sentence sentence, List<Center> centerList)
+        {
+            if (centerList.Count == 0)
+            {
+                
+                centerList.Add(Sentence2Center(sentence));
+
+                return centerList;
+            }
+            else
+            {
+                centerList = centerList.OrderBy(c => c.words.Count).ToList();
+                int iCur = 0;
+                bool isSimi = false;
+                while ( (iCur < centerList.Count)
+                    && (sentence.words.Count <= (sentence.words.Count / this.threshold)) )
+                {
+                    centerList[iCur] = UpdateCenter(sentence, centerList[iCur], out isSimi);
+                    iCur++;
+                    if (isSimi)
+                    {
+                        break;
+                    }
+                }
+                if (!isSimi)
+                {
+                    centerList.Add(Sentence2Center(sentence));
+                }
+
+                return centerList;
+            }            
+        }
+
+        private Center Sentence2Center(Sentence sentence)
+        {
+            Center center = new Center();
+            string name = "";
+            foreach (string s in sentence.words)
+            {
+                name += s;
+            }
+            center.clusterName = name;
+            center.hashcode = sentence.hashCode;
+            center.words = sentence.words;
+            string idSet = sentence.id.ToString() + ",";
+            center.idSet = idSet;
+            center.Count = 1;
+
+            return center;
+        }
+
+        private Center UpdateCenter(Sentence s, Center c, out bool isSimilar)
+        {
+            if (s.hashCode == c.hashcode)
+            {
+                c.idSet += s.id.ToString() + ",";
+                c.Count += 1;
+                isSimilar = true;
+                return c;
+            }
+            else
+            {
+                if (jaccardCoefficient(c.words, s.words) >= this.threshold)
+                {
+                    c.idSet += s.id.ToString() + ",";
+                    c.Count += 1;
+                    isSimilar = true;
+                }
+                else
+                {
+                    isSimilar = false;
+                }
+
+                return c;
+            }
+
+        }
+
+        private float jaccardCoefficient(HashSet<string> target, HashSet<string> tobeCluster)
+        {
+            //
+            int intersectionCount = 0;
+            //
+            foreach (string s in target)
+            {
+                if (tobeCluster.Contains(s))
+                {
+                    intersectionCount++;
+                }
+            }
+
+            return (float)intersectionCount / (float)(target.Count + tobeCluster.Count - intersectionCount);
+        }
+               
 
         private List<Sentence> GetUnque(List<Sentence> sentList)
         {
@@ -325,58 +467,7 @@ namespace ClusterLibrary
 
             return list1;
         }
-
-        protected virtual List<List<Sentence>> ComputeCluster(List<Sentence> sentenceList)
-        {
-            sentenceList = sentenceList.OrderBy(s => s.words.Count).ToList();
-            var ret = new List<List<Sentence>>();
-            // do computation here
-            while (sentenceList.Count > 0)
-            {
-                List<Sentence> curCluster = new List<Sentence>();
-                List<Sentence> left = new List<Sentence>();
-                Sentence curRef = sentenceList[0];
-                curCluster.Add(curRef);
-                for (int i = 1; i < sentenceList.Count; i++)
-                {
-                    if (curRef.words.Count < this.threshold * sentenceList[i].words.Count)
-                    {
-                        left.AddRange(sentenceList.GetRange(i, (sentenceList.Count - i)));
-                        break;
-                    }
-                    if (jaccardCoefficient(curRef.words, sentenceList[i].words) >= this.threshold)
-                    {
-                        curCluster.Add(sentenceList[i]);
-                    }
-                    else
-                    {
-                        left.Add(sentenceList[i]);
-                    }
-                }
-
-                ret.Add(curCluster);
-                sentenceList = left;
-            }
-
-            return ret;
-        }
-
-        private float jaccardCoefficient(HashSet<string> target, HashSet<string> tobeCluster)
-        {
-            //
-            int intersectionCount = 0;
-            //
-            foreach (string s in target)
-            {
-                if (tobeCluster.Contains(s))
-                {
-                    intersectionCount++;
-                }
-            }
-
-            return (float)intersectionCount / (float)(target.Count + tobeCluster.Count - intersectionCount);            
-        }
-
+       
         private int LoadConfigFile(string filePath)
         {
             try
